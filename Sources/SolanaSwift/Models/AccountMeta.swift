fac7ddf7730947216981f500d24185f2aac37af1
alanblockc <alanblockc@gmail.com>
2024-09-11 22:34:44 +0800

    Update AccountMeta.swift

--- Sources/SolanaSwift/Models/AccountMeta.swift
+++ Sources/SolanaSwift/Models/AccountMeta.swift
@@ -8,20 +8,20 @@ public struct AccountMeta: Equatable, Codable, CustomDebugStringConvertible {
     // MARK: - Decodable
 
     enum CodingKeys: String, CodingKey {
-        case pubkey, signer, isMut
+        case name, isSigner, isMut
     }
 
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        publicKey = try PublicKey(string: values.decode(String.self, forKey: .pubkey))
-        isSigner = try values.decode(Bool.self, forKey: .signer)
+        publicKey = try PublicKey(string: values.decode(String.self, forKey: .name))
+        isSigner = try values.decode(Bool.self, forKey: .isSigner)
         isWritable = try values.decode(Bool.self, forKey: .isMut)
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(publicKey.base58EncodedString, forKey: .pubkey)
-        try container.encode(isSigner, forKey: .signer)
+        try container.encode(publicKey.base58EncodedString, forKey: .name)
+        try container.encode(isSigner, forKey: .isSigner)
         try container.encode(isWritable, forKey: .isMut)
     }
 
